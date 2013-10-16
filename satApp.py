498fe4f4a0e7f9c02965ba7e942eef08bd0187e3
Thomas Liu <thomasliu02@gmail.com>
2013-10-16 16:13:00 -0500

    Added some methods mainly useful for debugging

--- satApp.py
+++ satApp.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     satGenerator = satGen.Generator()
 #    satProblem = satGenerator.uniformRandom(3,10,3)
     satProblem = satGenerator.generate()
+#    satProblem = satGenerator.parseText(['c[10, -6, 1]', 'c[4, 3, 2]', 'c[-4, -5, 9]', 'c[9, 9, -4]', 'c[4, -4, -9]', 'c[-3, -10, -8]', 'c[7, 7, 4]', 'c[4, -9, 6]', 'c[7, 3, 1]', 'c[-2, -1, -10]', 'c[-7, 2, 5]', 'c[-8, 9, 10]'])
     solver = simpleSat.Solver()
     solution = solver.solve(satProblem)
     if solution:
